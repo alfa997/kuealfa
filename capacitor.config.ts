2d4504514e3d3187ca585ee9270aed02965b80bd
alfa997 <daffaalfaro81@gmail.com>
2025-05-13 00:28:17 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'toko_kue_ulang_tahun',
+  webDir: 'www'
+};
+
+export default config;
